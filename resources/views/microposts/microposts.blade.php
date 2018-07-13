516a8c21353ad484a7495ca309a932ccf0fc0c81
EC2 Default User <ec2-user@ip-172-31-26-239.us-east-2.compute.internal>
2018-07-13 00:14:15 +0000

    kadai_micropost_img-circle

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -3,7 +3,7 @@
     <?php $user = $micropost->user; ?>
     <li class="media">
         <div class="media-left">
-            <img class="media-object img-rounded" src="{{ Gravatar::src($user->email, 50) }}" alt="">
+            <img class="media-object img-circle" src="{{ Gravatar::src($user->email, 50) }}" alt="">
         </div>
         <div class="media-body">
             <div>
